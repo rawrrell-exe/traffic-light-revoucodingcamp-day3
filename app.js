5f4347da63abfd3b48008288821e27ea08da2e49
rawrrell-exe <roro0110aurel@gmail.com>
2026-09-02 20:41:17 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// get variable
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const switchBtn = document.getElementById("switchBtn");
+
+// reset light
+function resetLights() {
+	red.style.background = "#444";
+	yellow.style.background = "#444";
+	green.style.background = "#444";
+}
+
+// initial value
+let current = "red";
+red.style.background = "red";
+
+// switch light
+function switchLight() {
+	switchBtn.disabled = true;
+	resetLights();
+
+	// Nyalain kuning dulu
+	yellow.style.background = "yellow";
+
+	setTimeout(() => {
+		resetLights();
+
+		if (current === "red") {
+			green.style.background = "lime";
+			current = "green";
+		} else {
+			red.style.background = "red";
+			current = "red";
+		}
+
+		switchBtn.disabled = false;
+	}, 1000); // kuning nyala 300ms
+}
+
+switchBtn.addEventListener("click", switchLight);
